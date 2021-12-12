e1cebebbed005a3ccc35c8dca99dd73a5ea1aeb0
Matt Payne <Payne@MattPayne.org>
2021-12-12 03:04:35 +0000

    added /clear route & tweaked formatting

--- app/forms.py
+++ app/forms.py
@@ -2,19 +2,16 @@ from flask_wtf import FlaskForm
 from wtforms import SelectMultipleField, StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
+
 class LoginForm(FlaskForm):
-  username = StringField('Username', validators=[DataRequired()])
-  password = PasswordField('Password', validators=[DataRequired()])
-  remember_me = BooleanField('Remember Me')
-  submit = SubmitField('Sign In')
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
 
 class ConsumeForm(FlaskForm):
-  thing = SelectMultipleField(u'Thing', 
-    choices=[('cpp', 'C++'), 
-    ('py', 'Python'), 
-    ('text', 'Plain Text')])
-  submit = SubmitField('Eat IT!')
-
-
-
- 
+    choices = [('coke', 'Coke'), ('celsius', 'Celsius Energy Drink'),
+               ('candy', 'Candy')]
+    thing = SelectMultipleField(u'Thing you eat next', choices=choices)
+    submit = SubmitField('Eat IT!')
--- app/routes.py
+++ app/routes.py
@@ -11,13 +11,18 @@ def index():
   form = ConsumeForm()
   users = web.UserStore()
   stuff = users.current.get('stuff', [])
-  # stuff = [web.auth.name]
   if form.thing.data:
     for x in form.thing.data:
       stuff.append(x)
   users.current['stuff'] = stuff
   return render_template('index.html', title='Home', stuff=stuff, form=form)
 
+@app.route('/clear', methods=['GET', 'POST'])
+@web.authenticated
+def clear():
+  users = web.UserStore()
+  del users.current['stuff']
+  return f"<h2>database for {web.auth.name} has its 'stuff' key removed.</h2>"
 
 @app.route('/eg', methods=['GET', 'POST'])
 def example():
