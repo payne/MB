213d1b7f17a5873724189d14a9c9dc34d629af94
Matt Payne <Payne@MattPayne.org>
2021-12-11 18:08:55 -0600

    Auth & db added

--- app/routes.py
+++ app/routes.py
@@ -2,25 +2,20 @@ from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 from app.forms import ConsumeForm
+from replit import db, web
 
 
 @app.route('/', methods=['GET', 'POST'])
+@web.authenticated
 def index():
   form = ConsumeForm()
-  stuff = ['this','that','other']
+  users = web.UserStore()
+  stuff = users.current['stuff']
   if form.thing.data:
     for x in form.thing.data:
       stuff.append(x)
+  users.current['stuff'] = stuff
   return render_template('index.html', title='Home', stuff=stuff, form=form)
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-  form = LoginForm()
-  if form.validate_on_submit():
-    flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-    return redirect(url_for('index'))
-  return render_template('login.html', title='Sign In', form=form)
 
 
 @app.route('/eg', methods=['GET', 'POST'])
