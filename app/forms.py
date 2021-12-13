66c0405aa809c44f641ee22d9f13e075b0603ec2
Matt Payne <Payne@MattPayne.org>
2021-12-13 01:18:19 +0000

    started the add money feature

--- app/forms.py
+++ app/forms.py
@@ -1,14 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import SelectMultipleField, StringField, PasswordField, BooleanField, SubmitField
+from wtforms import SelectMultipleField, StringField, PasswordField, BooleanField, SubmitField, DecimalField
 from wtforms.validators import DataRequired
 
+class DepositForm(FlaskForm):
+    amount = DecimalField('Money', validators=[DataRequired()])
+    submit = SubmitField('Record Payment')
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
-
 
 class ConsumeForm(FlaskForm):
     choices = [('coke', 'Coke'), ('celsius', 'Celsius Energy Drink'),
